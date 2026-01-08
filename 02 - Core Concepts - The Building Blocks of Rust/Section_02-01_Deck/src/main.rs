edee13ef0e97e7f9ef493646c684ed4d3bad5873
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-08 15:29:30 +0100

    02.7 Representing Data with Structs, 02.8 Adding Functionality to Structs (derive), 02.9 Arrays vs Vectors

--- 02 - Core Concepts - The Building Blocks of Rust/Section_02-01_Deck/src/main.rs
+++ 02 - Core Concepts - The Building Blocks of Rust/Section_02-01_Deck/src/main.rs
@@ -8,6 +8,21 @@ fn main() {
     // "println!" is called a macro
     println!("Hello, world!");
 
+    // List of 'suits' - "Hearts", "Spades", ...
+    // List of 'values' - "ace", "two", ...
+    // Both will be arrays because they probably won't change in size (otherwise, use a vec! for Vector)
+    let suits = ["Hearts", "Spades", "Diamonds"];
+    let values = ["Ace", "Two", "Three"];
+
+    // Double nested for loop
+    let cards = vec![];
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
     // Let's declare a new variable (in Rust, it's called a "binding" ~ so let's declare a new binding)
     let deck = Deck { cards: vec![] }; // This is writing using the macro
     // let deck = Deck { cards: Vec::new() }; // This is equivalent to the above line of code (this is written using the function)
