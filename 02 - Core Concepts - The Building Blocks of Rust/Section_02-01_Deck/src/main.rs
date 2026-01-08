f4912e2d409fb3497ea3f5a08c5eebd784fa903f
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-08 14:42:52 +0100

    02.7 Representing Data with Structs

---
+++ 02 - Core Concepts - The Building Blocks of Rust/Section_02-01_Deck/src/main.rs
@@ -0,0 +1,18 @@
+struct Deck {
+    cards: Vec<String>,
+}
+
+// In Rust, the "main" function will always be called when running the program
+fn main() {
+    // "println!" is called a macro
+    println!("Hello, world!");
+
+    // Let's declare a new variable (in Rust, it's called a "binding" ~ so let's declare a new binding)
+    let deck = Deck { cards: vec![] }; // This is writing using the macro
+    // let deck = Deck { cards: Vec::new() }; // This is equivalent to the above line of code (this is written using the function)
+
+    // Display your deck
+    println!("1) Here's your deck: {:?}", deck);
+    println!("2) Here's your deck: {}", deck);
+    println!("3) Here's your deck: {deck}");
+}
