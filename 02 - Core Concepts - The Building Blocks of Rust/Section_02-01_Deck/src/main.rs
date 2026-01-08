63e265b8b3d72a12f77c6d13f0a2a1300d511f53
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-08 15:46:34 +0100

    02.7 Representing Data with Structs, 02.8 Adding Functionality to Structs (derive), 02.9 Arrays vs Vectors, 02.10 Mutable vs Immutable Bindings

--- 02 - Core Concepts - The Building Blocks of Rust/Section_02-01_Deck/src/main.rs
+++ 02 - Core Concepts - The Building Blocks of Rust/Section_02-01_Deck/src/main.rs
@@ -7,6 +7,17 @@ struct Deck {
 fn main() {
     // "println!" is called a macro
     println!("Hello, world!");
+    println!(" ==================================== ");
+
+    // Let's declare a new variable (in Rust, it's called a "binding" ~ so let's declare a new binding)
+    let emptyDeck = Deck { cards: vec![] }; // This is writing using the macro
+    // let deck = Deck { cards: Vec::new() }; // This is equivalent to the above line of code (this is written using the function)
+
+    // Display your deck
+    println!("Here's your empty deck: {:?}", emptyDeck); // ":?" is a (debug) formatter
+    //println!("Here's your deck: {}", deck);
+    //println!("Here's your deck: {deck}");
+    println!(" ==================================== ");
 
     // List of 'suits' - "Hearts", "Spades", ...
     // List of 'values' - "ace", "two", ...
@@ -14,8 +25,9 @@ fn main() {
     let suits = ["Hearts", "Spades", "Diamonds"];
     let values = ["Ace", "Two", "Three"];
 
+    let mut cards = vec![];
+
     // Double nested for loop
-    let cards = vec![];
     for suit in suits {
         for value in values {
             let card = format!("{} of {}", value, suit);
@@ -23,12 +35,8 @@ fn main() {
         }
     }
 
-    // Let's declare a new variable (in Rust, it's called a "binding" ~ so let's declare a new binding)
-    let deck = Deck { cards: vec![] }; // This is writing using the macro
-    // let deck = Deck { cards: Vec::new() }; // This is equivalent to the above line of code (this is written using the function)
-
-    // Display your deck
-    println!("Here's your deck: {:?}", deck); // ":?" is a (debug) formatter
-    //println!("Here's your deck: {}", deck);
-    //println!("Here's your deck: {deck}");
+    // let deck = Deck { cards: cards}; // Because we have a field named with an identical value we're to assign to it, it can be shortened as the following line of code
+    let deck = Deck { cards };
+    println!("Here's your deck: {:#?}", deck); // Adding a "#" to the debug formatter will render it much better / easier to read.
+    println!(" ==================================== ");
 }
