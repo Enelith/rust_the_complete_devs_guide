a2f46e8132eecd131c2278f14210dd641a4f12a9
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-08 16:58:22 +0100

    02.15 Shuffling a Slice

--- 02 - Core Concepts - The Building Blocks of Rust/Section_02-02_Cleaner_Deck/src/main.rs
+++ 02 - Core Concepts - The Building Blocks of Rust/Section_02-02_Cleaner_Deck/src/main.rs
@@ -1,3 +1,10 @@
+// use rand::{thread_rng, seq::SliceRandom}
+// This is equivalent to defining an alias in order to use the external crate methods in our code:
+// use rand::thread_rng
+// use rand::seq::SliceRandom
+// Also, note that rand::thread_rng is deprecated since 0.9.0, and renamed 'rng'
+use rand::{rng, seq::SliceRandom};
+
 #[derive(Debug)] // Is required in order to display our deck using the debug formatter "{:?}"
 struct Deck {
     cards: Vec<String>,
@@ -31,14 +38,26 @@ impl Deck {
         deck // is EQUALS to "return deck;" ; we could even just write "Deck { cards }" instead of creating a binding "deck"
     }
 
-    fn shuffle(&self) {
+    // When calling shuffle, we're going to have a REFERENCE to our deck instance, and we're going to expect in this method to
+    // change the data inside that deck instance in some way (we want to randomize a list of cards).
+    // So, in addition to mark, in the 'main' function, the binding 'deck' with 'mut',
+    // we ALSO need to mark '&self' as '&mut self' (that's going to indicate that when we receive this REFERENCE
+    // to the deck we're going to change, we're going to get a MUTABLE / CHANGEABLE version of it).
+    // fn shuffle(&self) {...} -> fn shuffle(&mut self) {...}
+    fn shuffle(&mut self) {
+        // Creating a new random number generator by calling the crate 'rand'
+        // Equivalent to: let mut rng = rand::rng();
+        let mut rng = rng(); // ('mut' is also added here, bcs the data inside the 'rng' will also change in some way)
 
+        self.cards.shuffle(&mut rng)
     }
 }
 
 // In Rust, the "main" function will always be called when running the program
 fn main() {
-    let deck = Deck::new();
+    // Because we're going to shuffle our deck, the data inside 'deck' will change, and by default, 'deck' is immutable.
+    // Ergo, we need to qualify 'deck' with the keyword 'mut'
+    let mut deck = Deck::new();
 
     deck.shuffle();
 
