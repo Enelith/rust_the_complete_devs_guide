5b130a5ac1019d45e16ccd9391d6a3f6303f1b7c
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-12 10:15:23 +0100

    03.26 Exercise Overview, 03.27 Exercice Solution

---
+++ 03 - Ownership and Borrowing - Rust's Unique Memory System/Section_03-02_Exercice/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    balance: i32,
+    holder: String,
+}
+
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
+fn main() {
+    let account = Account::new(1, String::from("me"));
+
+    /*
+        TODO: Write and call a function that will *take ownership* of the Account
+         value, print it, and return nothing.
+         Question: Will you be able to call the function twice with the 'account'
+         variable?
+    */
+    print_account(account);
+    // Answer: No. Can't use the same variable to call the function twice
+}
+
+fn print_account(account: Account) {
+    println!("{:#?}", account)
+}
