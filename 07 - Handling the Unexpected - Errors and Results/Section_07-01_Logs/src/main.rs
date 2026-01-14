c6380a90bb2938e08242636f00441cb847ec3e95
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-14 16:14:45 +0100

    07.63 Reading a File

--- 07 - Handling the Unexpected - Errors and Results/Section_07-01_Logs/src/main.rs
+++ 07 - Handling the Unexpected - Errors and Results/Section_07-01_Logs/src/main.rs
@@ -1,3 +1,10 @@
+// 'std' = 'Standard Library', 'fs' = 'File System'
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let text = fs::read_to_string("logs.txt");
+
+    println!("{:#?}", text);
+    // Will return: "OK( 'INFO 14:30:25 Application started.\nWARN.... Network connectivity restored.',)"
+    // which is very similar to the previous Option enum (Some, None)
 }
