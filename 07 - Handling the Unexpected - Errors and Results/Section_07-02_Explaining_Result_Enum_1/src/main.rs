1a0f953b58af750d49312277f3cc9af26e92d32a
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-15 11:31:03 +0100

    07.67 Matching on Results

--- 07 - Handling the Unexpected - Errors and Results/Section_07-02_Explaining_Result_Enum_1/src/main.rs
+++ 07 - Handling the Unexpected - Errors and Results/Section_07-02_Explaining_Result_Enum_1/src/main.rs
@@ -1,7 +1,25 @@
 use std::io::Error;
 
 fn main() {
-    println!("Hello, world!");
+    // Will return the result: 1.666667
+    match divide(5.0, 3.0) {
+        Ok(result_of_division) => {
+            println!("{}", result_of_division);
+        }
+        Err(what_went_wrong) => {
+            println!("{}", what_went_wrong);
+        }
+    }
+
+    // Will return an error: Can't divide by 0
+    match divide(5.0, 0.0) {
+        Ok(result_of_division) => {
+            println!("{}", result_of_division);
+        }
+        Err(what_went_wrong) => {
+            println!("{}", what_went_wrong);
+        }
+    }
 }
 
 fn divide(a: f64, b: f64) -> Result<f64, Error> {
