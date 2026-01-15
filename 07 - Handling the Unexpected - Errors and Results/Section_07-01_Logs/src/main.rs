cc5cfb98b118cbcada9d0e6ad858c06366c7626b
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-15 14:34:55 +0100

    07.71 Using a Result when Reading Files

--- 07 - Handling the Unexpected - Errors and Results/Section_07-01_Logs/src/main.rs
+++ 07 - Handling the Unexpected - Errors and Results/Section_07-01_Logs/src/main.rs
@@ -7,4 +7,13 @@ fn main() {
     println!("{:#?}", text);
     // Will return: "OK( 'INFO 14:30:25 Application started.\nWARN.... Network connectivity restored.',)"
     // which is very similar to the previous Option enum (Some, None)
+
+    match text {
+        Ok(text_that_was_read) => {
+            println!("Text length: {}", text_that_was_read.len());
+        }
+        Err(why_this_failed) => {
+            println!("Failed to read file: {}", why_this_failed);
+        }
+    }
 }
--- 07 - Handling the Unexpected - Errors and Results/Section_07-03_Exercice_Result_Enum/src/main.rs
+++ 07 - Handling the Unexpected - Errors and Results/Section_07-03_Exercice_Result_Enum/src/main.rs
@@ -1,13 +1,15 @@
 use std::io::Error;
 
 // Todo: add in a return type
-fn validate_ingredients(ingredients: &Vec<String>) {
+fn validate_ingredients(ingredients: &Vec<String>) -> Result<(), Error> {
     if ingredients.len() > 3 {
         // Todo: make it clear that this is an error!
+        Err(Error::other("Too many ingredients"))
     } else {
         // Todo: make it clear that the ingredidents passed
         // validation. Note that we don't really have any value
         // to include in the 'Ok' here...
+        Ok(())
     }
 }
 
@@ -22,7 +24,12 @@ fn main() {
     // Todo: validation is an operation that might succeed or fail
     // Print out a success or fail message based on whether
     // it passes validation
-    validate_ingredients(&ingredients);
+    let validation = validate_ingredients(&ingredients);
+
+    match validation {
+        Ok(..) => println!("Ingredients are ok"),
+        Err(error) => println!("Error: {}", error),
+    }
 }
 
 
