f10764390e44537d3d255d3281af241f424ac07d
VIVERIS\j.vinh <jonathan.vinh@viveris.fr>
2026-01-26 15:09:32 +0100

    10.111 The Basics of Generics

--- 10 - Generics and Traits - Writing Flexible Reusable Code/README.md
+++ 10 - Generics and Traits - Writing Flexible Reusable Code/README.md
@@ -1,3 +1,21 @@
 # Generics and Traits
 
 Generics are a bit tricky, therefore the project inside this folder will be as simple and as to the point as possible.
+
+## The Basics of Generics
+
+```
+fn solve<T: Float>(a: T, b: T) -> T {
+	(a.powi(2) + b.powi(2)).sqrt()
+}
+```
+**Generic Type**: Like an argument list, but for types
+
+A generic type allows us to provide some customiation or really flexibility around not only functions, but methods, enums, vectors, structs.
+<br/>
+Just about everything in Rust can work with generics, and the goal of these generic types are to make your code a little bit more flexible and work with different types.
+
+The angle brackets `<T: Float>` is a generic list.
+<br/>
+You can think of this as being like an argument list, but specifically for types.
+
--- 10 - Generics and Traits - Writing Flexible Reusable Code/Section_10-01_Project_Generics/src/main.rs
+++ 10 - Generics and Traits - Writing Flexible Reusable Code/Section_10-01_Project_Generics/src/main.rs
@@ -1,7 +1,16 @@
-use num_traits::ToPrimitive;
+use num_traits::{Float, ToPrimitive};
 
 fn solve(a: f64, b: f64) -> f64 {
     (a.powi(2) + b.powi(2)).sqrt()
+}
+
+// First version: we can pass in BOTH f32 or f64
+// Second version: we can pass in any type of numbers
+fn solve_generic<T: Float>(a: T, b: T) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
 }
 
 fn main() {
@@ -12,5 +21,13 @@ fn main() {
     // Note that a.to_f64() returns an Option<f64>
     let a_f64 = a.to_f64().unwrap();
 
-    println!("{}", solve(a_f64, b));
+    println!("Solve: {}", solve(a_f64, b));
+
+    // ---------------
+
+    let a: f32 = 3.0;
+    let b: f32 = 4.0;
+
+    println!("Solve Generic: {}", solve_generic::<f32>(a, b));
+
 }
